97741abd6d7d4ce34bcd423a0c5e783e2b6f68aa
Paul Justin A. Segura <pauljustin.segura@wvsu.edu.ph>
2024-10-02 17:39:50 +0800

    Role Selection New Layout Design Exercise 4

--- lib/features/role_selection_page.dart
+++ lib/features/role_selection_page.dart
@@ -5,65 +5,127 @@ class RoleSelectionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 150.0,
-              height: 150.0,
-              child: Image.asset(
-                '../lib/assets/logo.png',
-                fit: BoxFit.contain,
-              ),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Welcome to',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const Text(
+                        'myHomy.',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.green,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: 300.0,
+                        height: 300.0,
+                        child: Image.asset(
+                          'lib/assets/role_selection_illustration.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        'Discover your ideal boarding house.',
+                        style: TextStyle(fontSize: 18, color: Colors.black54),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width *
+                            0.99, // to occupy 99% of screen width
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            side: const BorderSide(
+                                color: Colors.green, width: 2.0),
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.white,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24.0, vertical: 14.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                          onPressed: () {
+                            // Logic to identify type of user for boarder
+                            // Navigator.push(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //     builder: (context) => const SignUpPage(userType: 'Boarder'),
+                            //   ),
+                            // );
+                          },
+                          child: const Text(
+                            'Are you a user?',
+                            style: TextStyle(fontSize: 16, color: Colors.green),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width *
+                            0.99, // to occupy 99% of screen width
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.green,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24.0, vertical: 14.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                          onPressed: () {
+                            // Logic to identify type of user for owner
+                            // Navigator.push(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //     builder: (context) => const SignUpPage(userType: 'Owner'),
+                            //   ),
+                            // );
+                          },
+                          child: const Text(
+                            'Are you an owner?',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            const Text(
-              'Discover your ideal boarding house.',
-              style: TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                side: const BorderSide(color: Colors.blue, width: 2.0),
-                foregroundColor: Colors.blue,
-                backgroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 24.0, vertical: 12.0),
-              ),
-              onPressed: () {
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(
-                //     builder: (context) => const SignUpPage(userType: 'Boarder'),
-                //   ),
-                // );
-              },
-              child: const Text('Are you a boarder?'),
-            ),
-            const SizedBox(
-              height: 20,
-              width: 50,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                side: const BorderSide(color: Colors.blue, width: 2.0),
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.blue,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 24.0, vertical: 12.0),
-              ),
-              onPressed: () {
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(
-                //     builder: (context) => const SignUpPage(userType: 'Owner'),
-                //   ),
-                // );
-              },
-              child: const Text('Are you an owner?'),
-            ),
-          ],
+          ),
         ),
       ),
     );
